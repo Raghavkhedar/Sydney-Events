ebc9c35c95551f7cf65a0ac4221fa791682b76e7
Raghav Khedar <raghavkhedar528@gmail.com>
2026-02-04 16:07:14 +0530

    update urls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ const API = axios.create({
   // Prefer env var in production, fall back to your Render backend URL
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://sydney-events-backend-26jx.onrender.com/api",
+    "https://sydney-events-backendd.onrender.com/api",
 });
 
 export const fetchEvents = async (page = 1) => {
